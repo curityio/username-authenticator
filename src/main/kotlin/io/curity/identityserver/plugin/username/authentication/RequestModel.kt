bfad7ae27e0e427147f5bf173432ac1c1b994ab4
Daniel Lindau <d.lindau@gmail.com>
2021-05-12 14:54:02 +0200

    Add toggle to autopost preferred username

--- src/main/kotlin/io/curity/identityserver/plugin/username/authentication/RequestModel.kt
+++ src/main/kotlin/io/curity/identityserver/plugin/username/authentication/RequestModel.kt
@@ -1,17 +1,27 @@
 package io.curity.identityserver.plugin.username.authentication
 
 import org.hibernate.validator.constraints.NotBlank
+import se.curity.identityserver.sdk.service.OriginalQueryExtractor
+import se.curity.identityserver.sdk.service.UserPreferenceManager
 import se.curity.identityserver.sdk.web.Request
 import javax.validation.Valid
 
-class RequestModel(request: Request) {
+class RequestModel(request: Request, userPreferenceManager: UserPreferenceManager)
+{
     @Valid
-    val postRequestModel: Post? = if (request.isPostRequest) Post(request)
-    else
-        null
+    val postRequestModel: Post? = if (request.isPostRequest) Post(request) else null
+
+    @Valid
+    val getRequestModel: Get? = if (request.isGetRequest) Get(userPreferenceManager) else null
 }
 
-class Post(request: Request) {
+class Post(request: Request)
+{
     @NotBlank(message = "validation.error.username.required")
     val username: String = request.getFormParameterValueOrError("username")
+}
+
+class Get(userPreferenceManager: UserPreferenceManager)
+{
+    val preferredUserName: String? = userPreferenceManager.username
 }
